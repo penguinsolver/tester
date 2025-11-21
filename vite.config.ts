e062b0602c8ce8f56981511b6b7df5b074012948
penguinsolver <penguinsolver@gmail.com>
2025-11-21 23:16:29 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Belangrijk voor GitHub Pages: base moet matchen met je repo naam
+  base: '/tester/',
 })
